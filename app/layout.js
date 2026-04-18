f98194a0980e90cd0a88912caab4f1963c1ca4bc
Adeeba <adeebanisar@Adeebas-MacBook-Air.local>
2026-04-19 00:39:43 +0530

    google console

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="gZzWApKauN5jGYrNtGJIHXqGWZcAOFnHkyW_JroZUag" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Bricolage+Grotesque:wght@700;800&display=swap" rel="stylesheet" />
